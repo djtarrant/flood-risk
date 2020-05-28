17f37953d4d63b9d6b6208cd81a143f71871392e
Darren Tarrant <in@djtarrant.co.uk>
2020-05-28 18:48:29 +0100

    Csv processing

--- data-import/import.py
+++ data-import/import.py
@@ -5,25 +5,28 @@ import csv
 
 #vars
 fname = 'open_flood_risk_by_postcode.csv'
-floodRiskData = []
+included_cols = [0, 2, 4, 8, 9]
 
 # get a sample of data
-with open(fname, 'r') as f:
-    for lnum, line in enumerate(f):
-        if lnum > 10:
-            break
-        print(line[:-1])
+#with open(fname, 'r') as f:
+    #for lnum, line in enumerate(f):
+        #if lnum > 100:
+            #break
+        #print(line[:-1])
 
 #get the number of rows
-with open(fname, 'r') as f:
-    print( sum(1 for line in f) )
-
 #with open(fname, 'r') as f:
-    #reader = csv.DictReader(f)
-    #for row in reader:
-        #floodRiskData.append(row)
+    #print( sum(1 for line in f) )
 
-#for dataLine in floodRiskData:
-    #print(dataLine)
+#parse csv
+with open(fname, 'r') as fp:
+    reader = csv.reader(fp, delimiter=",")
+    for row in reader:
+        content = list(row[i] for i in included_cols)
+        if content[1] != 'None': #gets any at risk of flooding
+            print(content)
+    
 
 
+#attribution required
+#Contains Environment Agency data licensed under the Open Government Licence v3.0 (http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/).

d402f9f8dbd7d3e169791d363d795a4d0eb3b74e
Darren Tarrant <in@djtarrant.co.uk>
2020-06-04 17:46:18 +0100

    Search postcode endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from db import myconnection, mycursor, conn
 from flask import Flask, url_for, jsonify, request
+import json
 
 app = Flask(__name__)
 
@@ -30,6 +31,37 @@ def get_floodRisk(postcode):
     except conn.Error as error:
         message = "Failed to get record from MySQL table: {}".format(error)
         returnVars = jsonify(postcode=postcode, message=message)
+        status = 404
+    return returnVars, status
+
+#read
+@app.route('/searchPostcode/<string:postcodeText>', methods = ['GET'])
+def get_postcode(postcodeText):
+    sql = "SELECT postcode FROM location WHERE replace(postcode, ' ', '') LIKE %s LIMIT %s"
+    val = ('%'+postcodeText.replace(" ", "")+'%', 100)
+    print(val)
+    mycursor.execute(sql, val)   
+    try:
+        rows = mycursor.fetchall()
+        if len(rows) > 0:
+            recordDictionary = {}
+            num=0
+            for row in rows:
+                recordDictionary[num] =  row[0] 
+                print(row[0], num)
+                num=num+1
+            records = json.dumps(recordDictionary)
+            records = records.replace("\\", "")
+            message = "Total number of rows is: "+str(mycursor.rowcount)
+            returnVars = jsonify(postcodeSearch=postcodeText, message=message, records=recordDictionary)
+            status = 200
+        else:
+            message = "Total number of rows is: "+str(mycursor.rowcount)
+            returnVars = jsonify(postcodeSearch=postcodeText, message=message)
+            status = 404
+    except conn.Error as error:
+        message = "Failed to get records from MySQL table: {}".format(error)
+        returnVars = jsonify(postcodeSearch=postcodeText, message=message)
         status = 404
     return returnVars, status
 
